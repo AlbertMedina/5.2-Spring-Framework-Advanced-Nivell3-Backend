76779ed1379ba68a251aa9ef35f5e96584cad4f1
Albert Medina <albertmecu@gmail.com>
2026-01-22 20:14:13 +0100

    feat: added exists method to domain favourite repository

--- src/main/java/com/videostore/videostore/domain/repository/FavouriteRepository.java
+++ src/main/java/com/videostore/videostore/domain/repository/FavouriteRepository.java
@@ -1,12 +1,16 @@
 package com.videostore.videostore.domain.repository;
 
 import com.videostore.videostore.domain.model.Favourite;
+import com.videostore.videostore.domain.model.Movie;
+import com.videostore.videostore.domain.model.User;
 
 import java.util.List;
 
 public interface FavouriteRepository {
-    
+
     List<Favourite> findByUser(Long userId);
+
+    boolean exists(User user, Movie movie);
 
     Favourite save(Favourite favourite);
 
