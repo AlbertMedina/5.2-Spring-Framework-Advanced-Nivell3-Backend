12d569ede9e164ac1e8149a57a3d0258d8fef63a
Albert Medina <albertmecu@gmail.com>
2026-02-16 11:46:44 +0100

    feat: added 5173 (vite) port to cors configuration

--- src/main/java/com/videostore/videostore/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/videostore/videostore/infrastructure/config/SecurityConfig.java
@@ -81,7 +81,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "http://localhost:3000"
+        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         config.setAllowCredentials(true);
